93633c1ff9a90a2ded22b7238f55c26998fee829
jiyuujin <jiyuujinunite@gmail.com>
2024-01-20 11:52:56 +0900

    connpass initialize

---
+++ src/app.d.ts
@@ -0,0 +1,29 @@
+export interface Series {
+  id: number
+  title: string
+  url: string
+}
+
+export interface Event {
+  event_id: number
+  title: string
+  catch: string
+  description: string
+  event_url: string
+  started_at: string
+  ended_at: string
+  limit: number
+  hash_tag: string
+  event_type: string
+  accepted: number
+  waiting: number
+  updated_at: string
+  owner_id: number
+  owner_nickname: string
+  owner_display_name: string
+  place: string
+  address: string
+  lat: string
+  lon: string
+  series: Series
+}
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const connpassApiBase = 'https://connpass.com/api/v1/event/';
+
+// v-kansai series id
+export const seriesId = 6469;
