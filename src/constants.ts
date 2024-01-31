d39373c26a1828e43d2377506ccaf4314fcf2717
jiyuujin <jiyuujinunite@gmail.com>
2024-01-31 22:19:41 +0900

    kansai.ts test

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,6 @@ export const connpassApiBase = 'https://connpass.com/api/v1/event/';
 
 // v-kansai series id
 export const seriesId = 6469;
+
+// kansai.ts series id
+// export const seriesId = 7928;
